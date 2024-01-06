5870c0aae9be4993ae6a72b2f451494a7d7e2957
12ants <leonljunghorn@gmail.com>
2024-01-06 16:57:35 +0100

    update cccc.sh

--- sh/cccc.sh
+++ sh/cccc.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-###########
-########### /etc/cccc.sh
-########### functions for bash shell by ants
-###########
-## coolas
-coolas() { 
-echo;echo ""'' COLOR CODES. - USAGE:  ____echo -ne '"' \033[0000m'"'XXXX'"'\033[0m'"'____''"";echo; for i in {1..111}; do echo -ne "\033["$i"m"$((i+2))"\033[0m "; echo -ne "\033[0m+ \033[0m"; done; echo -n "$re";echo; 
-}
-###########
-###########
-###########
-###########
-###########
-###########
-###########
-###########
-###########
-###########
-###########
+echo cccc
