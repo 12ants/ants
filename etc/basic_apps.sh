ea38990a3203971997c981f9b12c8e8e4302f678
Leon Ljunghorn <leonljunghorn@gmail.com>
2024-01-19 17:45:47 +0100

    Update basic_apps.sh

--- etc/basic_apps.sh
+++ etc/basic_apps.sh
@@ -7,20 +7,7 @@
 ##############
 tput cuu 8; tput ed; ee -e "\n\n$ll$c2 Installing basic apps . . . \n\n";
 apt update; apt upgrade -y;
-apt install -y \
-gpm \
-links2 \
-ranger \
-mc \
-pv \
-neofetch \
-fortune-mod \
-tlp \
-btop \
-curl \
-lolcat \
-googler \
-2>/dev/null ;
+apt install -y gpm links2 ranger mc pv neofetch fortune-mod tlp btop curl lolcat googler 2>/dev/null;
 tput cuu 8; tput ed; ee -e "\n\n$ll$c2 Basic apps installed . . . \n\n";
 #### nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash 
