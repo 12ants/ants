1995d26689a0e73a3139de26a055a2811d42462d
Leon Ljunghorn <leonljunghorn@gmail.com>
2024-02-04 14:40:41 +0100

    Update ants.sh

--- ants.sh
+++ ants.sh
@@ -18,15 +18,16 @@ left2=$(tput cub 2) up1=$(tput cuu1)
 export dddd=$(echo -e "$pink  --------------------------------$re ") c2="$cyan --$re" ll=$(echo -e " \t\t ") 2>/dev/null \
 ##
 ants="$_"
-# tput cuu 18 ed; echo;echo -e " \n\n\t version: $(ls -n 2>/dev/null) ";echo;
-# read -n1 -rep "$ll""$c2"" Update Installer? "$dim"["$re$bold"Y"$dim"/"$re$bold"n"$re$dim"] $re" "yn"; 
-# if [ "$yn" != "${yn#[Nn]}" ]; then echo "$c2 nope"; else echo "$ll$c2 OK"; 
-# read -rep "$ll""$c2"" To: $re" -i "$PWD$_" "ants"; 
-# wget -O "$ants" https://raw.githubusercontent.com/12ants/ants/main/ants.sh;
-# bash "$ants"
-# exit 0
-# fi 
-# echo GG
+##
+## LOADING-ANIMATION
+pro() {
+$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2; PROC_ID=$!; while kill -0 "$PROC_ID"&>/dev/null; 
+do for X in "[        ]" "[$green=$re       ]" "[$yellow==$re      ]" "[$red===$re     ]" "[=$pink===    $re]"  "[ ==$cyan==   $re]" \
+"[   =$white===$re ]" "[     =$blue==$re]" "[      "$rev"=$re$green=$re]" "[       "$red"="$re"]" "[        ]" "[        ]" "[        ]" "[        ]"; 
+do echo -e "Executing $rev$pro$re"$c2" $X"; tput cuu1; sleep 0.08; done; echo -ne "oo"; done 
+echo -ne " [  "$green"DONE"$re"  ] \n\n\n\n\n";
+}
+##
 ##
 echo -e "\n\n"; ll=$(echo -e " \t\t "); 
 if [ $UID != 0 ]; then echo -e " \n\n $ll This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; read -ep "$ll k" "k7"; exit 0 ; fi 
@@ -38,33 +39,17 @@ echo -e "$SUDO_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ants;
 ##
 # snap install lolcat &>/dev/null & disown; tput cuu 2; tput ed
 # apt update|lolcat -F .01 -s 111 -p 11 -d 22; apt upgrade -y --install-recommends|lolcat -F .001 -s 111 -p 11 -d 22;
-##
+## 
 pro='apt update';
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
+pro;
+
+pro='sleep 2';
+
 pro;
 sleep 1;
 ##
 ##
-pro='apt upgrade -y';
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
-##
-##
-pro;
-sleep 1;
+pro='apt upgrade -y'; pro; sleep 1;
 ##
 ##
 tput cup 0; tput ed; echo -e "\n\n\t$blink ¯\(ツ)/¯$re ";
@@ -90,27 +75,11 @@ cd ants
 ##
 ## LOAD ANIMATION
 pro='git stash'
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
 pro
 ##
 ####
 ## LOAD ANIMATION
 pro='git pull'
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
 pro
 ##
 ##
@@ -121,73 +90,32 @@ sleep 1
 ####
 ## LOAD ANIMATION
 pro='git clone https://github.com/12ants/ants'
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
 pro
+sleep 1
 ##
 ##
-
-
-
-cd ants 
+cd ants;
 read -n1 -ep "$ll""$c2"" Install Improvments? "$dim"["$re$bold"Y"$dim"/"$re$bold"n"$re$dim"] $re" "yn"; 
 if [ "$yn" != "${yn#[Nn]}" ]; then echo "$c2 nope"; return 2>/dev/null; else echo "$ll$c2 OK";
-tput cuu 8 ed;
-tput dim;
+##
+tput cuu 18 ed; tput dim;
 sudo cp sh/aaaa.sh /etc/aaaa.sh -bv
 sudo cp sh/bbbb.sh /etc/bbbb.sh -bv
 sudo cp sh/cccc.sh /etc/cccc.sh -bv
 sudo cp sh/etc_profile.sh /etc/profile -bv
 sudo cp sh/etc_bash.sh /etc/bash.bashrc -bv
 sudo cp sh/ssss.sh /bin/ssss -bv
-echo; sleep 1
+echo "$re"; sleep 1;
 ##
 pro='chmod 775 /bin/ssss';
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
 pro;
-sleep 1;
-##
 ##
 ##
 pro='chown "$SUDO_USER": /etc/*.sh';
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
 pro;
-sleep 1;
 ##
 ##
-##
-pro='chmod 755 /etc/*.sh -v';
-pro() {
-$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2;
-PROC_ID=$!
-while kill -0 "$PROC_ID"&>/dev/null; do for X in "[      ]" "[$green=$re     ]" "[$green==$re    ]" "[$green===$re   ]" "[   $green===$re]" \
-"[    $green==$re]" "[     $green=$re]" "[      ]" "[      ]" "[      ]" "[      ]"; 
-do echo -e "\t $X$c2  $pro"; tput cuu1; sleep 0.08; done; done 
-echo -ne "\t\t\t\t  PROCESS ["$green"DONE"$re"]    \t\n\n\n\n\n";
-}
-pro;
-sleep 1;
-##
+pro='chmod 755 /etc/*.sh -v'; pro; sleep 1;
 ##
 fi;
 sleep 1; tput sgr0; 
