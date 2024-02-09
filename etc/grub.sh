12fabe2ea5177a193c9bf3c5dca57966e41719ad
12ants <leonljunghorn@gmail.com>
2024-02-09 17:40:41 +0100

    chore: delete balias.sh

--- etc/grub.sh
+++ etc/grub.sh
@@ -4,14 +4,13 @@
 ## About: Grub sh script.
 ##
 #grub
-mkdir -p -m 775 /boot/grub/ ;  cd /boot/grub/ ; 
-wget -O tard.jpg https://github.com/12ants/00/raw/main/img/tard2.jpg;
-mkdir -p -m 775 "/home/$SUDO_USER/ants/bu/";
-mv /etc/default/grub.d/* $ghh/bups0000  /bu/;
+mkdir -p -m 775 /boot/grub 2>/dev/null;
+wget -O /boot/grub/tard.jpg https://github.com/12ants/00/raw/main/img/tard2.jpg;
+mkdir -p -m 775 "/home/bups/"; mv /etc/default/grub.d/* /home/bups/ 2>/dev/null;
 echo '
 GRUB_BACKGROUND="/boot/grub/tard.jpg"
 GRUB_GFXMODE=auto
-#GRUB_TERMINAL_OUTPUT="gfxterm"
+GRUB_TERMINAL_OUTPUT="console"
 GRUB_DEFAULT=saved
 GRUB_SAVEDEFAULT=true
 GRUB_TIMEOUT_STYLE=menu
@@ -20,7 +19,7 @@ GRUB_DISTRIBUTOR=`lsb_release -dcs 2> /dev/null || echo Debian`
 GRUB_DISABLE_OS_PROBER=false
 GRUB_HIDDEN_TIMEOUT=0
 #GRUB_CMDLINE_LINUX="console" 
-#GRUB_CMDLINE_LINUX_DEFAULT="gfxterm" 
+GRUB_CMDLINE_LINUX_DEFAULT="console" 
 GRUB_GFXPAYLOAD_LINUX=keep
 ' > /etc/default/grub; 
 update-grub ; 
