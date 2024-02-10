c23f64a6c815808b499dd93ed03b78230e950dbc
12ants <leonljunghorn@gmail.com>
2024-02-10 16:46:42 +0100

    update ants.sh

--- ants.sh
+++ ants.sh
@@ -1,113 +1,66 @@
 #!/bin/bash
 ########### [ants.sh] - bash installer
 ########### 
-###########
-echo hello
-##
-##
 ## About: antsinstaller
 ############# 12ants.github.com
-#
-#
-tput cup 0; tput ed; echo -e "\n\n\t$blink ¯\(ツ)/¯$re ";
-alias "ee"='echo '
+tput cup 0; tput ed; echo -e "\n\n\t\t$blink ¯\(ツ)/¯$re ";
+alias "ee"='echo ';
 export bold=$(tput bold) dim=$(tput dim) so=$(tput smso) noso=$(tput rmso) rev=$(tput rev) re=$(tput sgr0) normal=$(tput sgr0) \
 redb=$(tput setab 1) greenb=$(tput setab 2) yellowb=$(tput setab 3) blueb=$(tput setab 4) purpleb=$(tput setab 5) cyanb=$(tput setab 6) \
 grayb=$(tput setab 7) red=$(tput setaf 1) green=$(tput setaf 2) yellow=$(tput setaf 3) blue=$(tput setaf 4) purple=$(tput setaf 5) \
 cyan=$(tput setaf 6) gray=$(tput setaf 7) white=$(tput setaf 7 bold) pink=$(tput setaf 5 bold) darkblue=$(tput setab 5 bold) blink=$(tput blink) \
 left2=$(tput cub 2) up1=$(tput cuu1) 
-export dddd=$(echo -e "$pink  --------------------------------$re ") c2="$cyan --$re" ll=$(echo -e " \t\t ") 2>/dev/null \
-##
-ants="$_"
-##
-## LOADING-ANIMATION
-#pro() {
-#$pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2; PROC_ID=$!; while kill -0 "$PROC_ID"&>/dev/null; 
-#do for X in "[        ]" "[$green=$re       ]" "[$yellow==$re      ]" "[$red===$re     ]" "[=$pink===    $re]"  "[ ==$cyan==   $re]" \
-#"[   =$white===$re ]" "[     =$blue==$re]" "[      "$rev"=$re$green=$re]" "[       "$red"="$re"]" "[        ]" "[        ]" "[        ]" "[        ]"; 
-#do echo -e "Executing $rev$pro$re"$c2" $X"; tput cuu1; sleep 0.08; done; echo -ne "oo"; done 
-#echo -ne " [  "$green"DONE"$re"  ] \n\n\n\n\n";
-#}
+export dddd=$(echo -e ""$pink"--------------------------------$re") c2=""$cyan"--"$re""; ants="$_"; ll=$(echo -e " \t "); 
 ###########
-## pro - task loaading animation
+if [ $UID != 0 ]; then echo -e " \n\n $ll This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; 
+read -ep "$ll K" "k7"; exit 0 ; fi;
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf 2>/dev/null;
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-installer; echo -e "$SUDO_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ants;
+###########
+## pro - task loading animation
 pro() {
-echo -e "\n\n\n\n\n\n\n\n\n\n\n\n"
-alias tf='tput setaf $((RANDOM%16));'
-alias tb='tput setab $((RANDOM%16));'
-c2=""$cyan"--$re"; tput civis;
-$pro &>/dev/null & disown; tput cuu 8; tput ed; PROC_ID=$!; while kill -0 "$PROC_ID"&>/dev/null; 
+tf() {
+tput setaf $((RANDOM%16));
+}
+tb() {
+tput setab $((RANDOM%16));
+}
+c2=""$cyan"--"$re""; tput civis; tput sgr0; 
+$pro &>x & disown; tput cuu1; PROC_ID=$!; while kill -0 "$PROC_ID" &>/dev/null; 
 do for X in "[        ]" "[$(tf)=$re       ]" "[$(tf)=$(tf)=$re      ]" "[$(tf)=$(tf)=$(tf)=$re     ]" "[$(tf)=$(tf)=$(tf)=$(tf)=    $re]"  \
 "[ $(tf)=$(tf)=$(tf)=$(tf)=   $re]" "[  $(tf)=$(tf)=$(tf)=$(tf)=$re  ]" "[   $(tf)=$(tf)=$(tf)=$(tf)= $re]" "[    $(tf)=$(tf)=$(tf)=$(tf)=$re]" \
 "[     "$(tf)"=$(tf)=$(tf)=$re]" "[      "$(tf)"=$(tf)="$re"]" "[       $(tf)=]" "[        ]" "[        ]" "[        ]"; 
-do echo -e "   $dim[$(tb)  $re$dim]$re "$c2" Executing $rev $pro $re $c2$c2$c2$c2$c2"; tput cuu1; echo -e "\t\t\t\t\t $X"; tput cuu1; sleep 0.08; done; done;
-echo -e "\t\t\t\t\t"$dim" [$re  "$green"DONE"$re" $dim ]$re \n\n\n\n\n"; tput cnorm;
+do echo -e "    $dim[$(tb)  $re$dim]$re "$c2" Executing $rev $pro $re $c2$c2$c2$c2$c2"; tput cuu1; tput sgr0;
+echo -e "\t\t\t\t\t $X"; tput cuu1; sleep 0.08; tput sc; tput cup $((LINES-4)) 0; echo -e "\t$rev $(tail -n2 x|head -n1) $re"; 
+echo -e "\t$rev $(tail -n1 x) $re"; tput cuu 2;
+tput rc; done; done;
+echo -e "\t\t\t\t\t"$dim" [$re  "$green"DONE"$re" $dim ]$re "; tput cnorm; echo;
 }
 ##
+tput cup 9;
+pro='apt update'; pro; sleep 1;
+pro='apt upgrade'; pro; sleep 1;
 ##
-echo -e "\n\n"; ll=$(echo -e " \t\t "); 
-if [ $UID != 0 ]; then echo -e " \n\n $ll This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; read -ep "$ll k" "k7"; exit 0 ; fi 
-## Autorestart as needed
-sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf 2>/dev/null;
-## - Auto root login for admins
-echo "%sudo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-installer;
-echo -e "$SUDO_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ants;
-##
-# snap install lolcat &>/dev/null & disown; tput cuu 2; tput ed
-# apt update|lolcat -F .01 -s 111 -p 11 -d 22; apt upgrade -y --install-recommends|lolcat -F .001 -s 111 -p 11 -d 22;
-## 
-pro='apt update';
-pro; sleep 1;
-##
-##
-pro='apt upgrade -y'; pro; sleep 1;
-##
-##
-#
-# Welcome to ...
-#
-echo -e "\n\n\n\n\n\n\t\t$dddd\n\t\t  Welcome to$cyan 12ants$re bash-improver! \n\t\t$dddd"; 
-####
-###
-###       USE FUNCTION ONLY ONCE!!!!
-##
-## Do you wish to proceed?
-##
+## Welcome to ... 		########
+echo -e "\t $dddd\n\t Welcome to$cyan 12ants$re bash-improver! \n\t $dddd"; 
+## Proceed ... 			########
 read -n1 -ep "$ll""$c2"" Do you wish to proceed? "$dim"["$re$bold"Y"$dim"/"$re$bold"n"$re$dim"] $re" "yn"; 
 if [ "$yn" != "${yn#[Nn]}" ]; then echo "$c2 nope";exit 1; else echo "$ll$c2 OK"; fi ;
-##
-read -ep "$ll""$c2"" Folder for Github? $re" -i "$PWD/gh" "gh"; 
-mkdir $gh -p -m 775
-chown $SUDO_USER: $gh
-cd $gh
-mkdir ants -p -m 775
-cd ants;
-pro='git stash'; pro; sleep 1;
-pro='git pull'; pro; sleep 1;
-cd ..; sleep 1;
-pro='git clone https://github.com/12ants/ants'; pro; sleep 1;
+## Github folder... 	########
+read -ep "$ll$c2 Folder for$cyan$bold Github? $re" -i "$PWD/gh/" "gh"; 
+install 
+mkdir $gh -p -m 775; chown $SUDO_USER: $gh; cd $gh; mkdir ants -p -m 775;
+cd ants; echo;echo;
+pro='git stash'; pro; sleep 1; pro='git pull'; pro; sleep 1; cd ..; sleep 1; echo -ne " $re ";
+pro="git clone https://github.com/12ants/ants"; pro; sleep 1;
 cd ants; read -n1 -ep "$ll""$c2"" Install Improvments? "$dim"["$re$bold"Y"$dim"/"$re$bold"n"$re$dim"] $re" "yn"; 
 if [ "$yn" != "${yn#[Nn]}" ]; then echo "$c2 nope"; return 2>/dev/null; else echo "$ll$c2 OK";
 ##
-tput cuu 18 ed; tput dim;
-sudo cp sh/aaaa.sh /etc/aaaa.sh -bv
-sudo cp sh/bbbb.sh /etc/bbbb.sh -bv
-sudo cp sh/cccc.sh /etc/cccc.sh -bv
-sudo cp sh/etc_profile.sh /etc/profile -bv
-sudo cp sh/etc_bash.sh /etc/bash.bashrc -bv
-sudo cp sh/ssss.sh /bin/ssss -bv
-echo "$re"; sleep 1;
-##
-pro='chmod 775 /bin/ssss'; pro; sleep 1;
-##
-##
-pro='chown "$SUDO_USER": /etc/*.sh'; pro;
-##
-##
-pro='chmod 755 /etc/*.sh -v'; pro; sleep 1;
-##
-fi;
+tput dim; sudo cp sh/aaaa.sh /etc/aaaa.sh -bv; sudo cp sh/bbbb.sh /etc/bbbb.sh -bv; sudo cp sh/cccc.sh /etc/cccc.sh -bv; 
+sudo cp sh/etc_profile.sh /etc/profile -bv; sudo cp sh/etc_bash.sh /etc/bash.bashrc -bv; sudo cp sh/ssss.sh /bin/ssss -bv;
+echo " $re "; sleep 1; pro='chmod 775 /bin/ssss'; pro; sleep 1; pro="chown "$SUDO_USER": /etc/*.sh"; pro; 
+pro='chmod 755 /etc/*.sh -v'; pro; sleep 1; fi;
 sleep 1; tput sgr0; 
-echo -e "\n\n\n\n    $ll $c2$blink Bash is now better! \n\n\n\n\n\n"; sleep 2;
+echo -e "\n\n\n\n    $ll $c2$blink Bash is now better! $re$c2 \n\n\n\n"; sleep 1;
 source multi.sh
-
-
+rm ./x;
---
+++ multi.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+## MULTISELECT BASH-MENU
+##
+## To run this script: -first declare the options:
+## " ops=(FIRST SECOND ETC) && source multi "
+##
+##   That's it! 
+##
+tput cup 2 1 ed; echo "
+  ------------------------------------------
+  ------------ $green hello $re ---------------------
+  ------------------------------------------ 
+  --$dim Choose:  [$re Up / Down$dim ]$re
+  --$dim Select:  [$re Enter$dim ]$re
+  --$dim Confirm: [$re Space$dim ]$re
+  ------------------------------------------
+  -- Choose multiple options: --------------
+  ------------------------------------------
+ "
+## func "multiselect" begin ############
+function multiselect {
+# little helpers for terminal print control and key input
+ESC=$( printf "\033")
+cursor_blink_on()   { printf "$ESC[?25h"; }
+cursor_blink_off()  { printf "$ESC[?25l"; }
+cursor_to()         { printf "$ESC[$1;${2:-1}H"; }
+print_inactive()    { printf "$2   $1 "; }
+print_active()      { printf "$2  $ESC[7m $1 $ESC[27m"; }
+get_cursor_row()    { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+local return_value=$1
+local -n options=$2
+local -n defaults=$3
+local selected=()
+for ((i=0; i<${#options[@]}; i++)); do
+if [[ ${defaults[i]} = "true" ]]; then
+selected+=("true")
+else
+selected+=("false")
+fi
+printf "\n"
+done
+# determine current screen position for overwriting the options
+local lastrow=`get_cursor_row`
+local startrow=$(($lastrow - ${#options[@]}))
+# ensure cursor and input echoing back on upon a ctrl+c during read -s
+trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+cursor_blink_off
+key_input() {
+local key
+IFS= read -rsn1 key 2>/dev/null >&2
+if [[ $key = ""      ]]; then echo space; fi;
+if [[ $key = $'\x20' ]]; then echo enter; fi;
+if [[ $key = "k" ]]; then echo up; fi;
+if [[ $key = "j" ]]; then echo down; fi;
+if [[ $key = $'\x1b' ]]; then
+read -rsn2 key
+if [[ $key = [A || $key = k ]]; then echo up;    fi;
+if [[ $key = [B || $key = j ]]; then echo down;  fi;
+fi
+}
+toggle_option() {
+local option=$1
+if [[ ${selected[option]} == true ]]; then
+selected[option]=false
+else
+selected[option]=true
+fi
+}
+print_options() {
+# print options by overwriting the last lines
+local idx=0
+for option in "${options[@]}"; do
+local prefix="        $green[$re $green]$re"
+if [[ ${selected[idx]} == true ]]; then
+prefix="        [\e[38;5;46m✔\e[0m]"
+fi
+cursor_to $(($startrow + $idx))
+if [ $idx -eq $1 ]; then
+print_active "$option" "$prefix"
+else
+print_inactive "$option" "$prefix"
+fi
+((idx++))
+done
+}
+local active=0
+while true; do
+print_options $active
+# user key control
+case `key_input` in
+space)  toggle_option $active;;
+enter)  print_options -1; break;;
+up)     ((active--));
+if [ $active -lt 0 ]; then active=$((${#options[@]} - 1)); fi;;
+down)   ((active++));
+if [ $active -ge ${#options[@]} ]; then active=0; fi;;
+esac
+done
+# cursor position back to normal
+cursor_to $lastrow
+printf "\n"
+cursor_blink_on
+eval $return_value='("${selected[@]}")'
+}
+##
+##
+opts="/tmp/opts1.sh"; echo "">$opts;
+ops=($(ls ./etc))
+pre=false
+#result=(  "res1" "res2" "res3")
+multiselect result ops pre
+echo "  ------------------------------------------
+"
+for index in "${!result[@]}";
+do
+echo -e "  ${ops[index]} ->\t\t\t\t\t ${result[$index]}"
+done
+echo "
+  ------------------------------------------
+";
+########
+read -n1 -ep "$(tput setaf 7 bold)
+  --  Are these changes correct? $(tput setaf 7)[Y$(tput dim)/$(tput sgr0)n$(tput setaf 7)]$(tput setaf 7 bold) " yn;
+if [ "$yn" != "${yn#[Nn]}" ]; then
+reset; source multi;
+else
+## write to file
+for index in "${!result[@]}";
+do
+echo -e "  ${ops[index]} ->\t ${result[$index]}" >> $opts
+done
+printf "\n ---- \n\n" >> $opts;
+## write to file
+tput cuu1; echo -e "\t\t\t\t\t\t[$green OK$re$dim ] 
+  --  variables written to: [$re./$opts$dim]";
+fi
+##########
+idx=0
+for option in "${my_options[@]}"; do
+printf  -v "$option" "${result[idx]}"  
+printf "$option=${result[idx]}\n" >> $opts
+((idx++)) 
+done
+tput sgr0;
+echo;echo;
