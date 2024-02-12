46a243ed36c63cd33fa4ab48560216ac4bf437ae
Leon Ljunghorn <leonljunghorn@gmail.com>
2024-02-12 16:54:51 +0100

    Update shpass.sh

--- sh/shpass.sh
+++ sh/shpass.sh
@@ -3,4 +3,4 @@
 echo;echo;read -ep " $c2 ssh host: " "host"; read -ep " $c2 ssh user: " "user";
 mkdir ~/.ssh 2>/dev/null; chmod 700 ~/.ssh; sudo chmod 700 ~/.ssh; cd ~/.ssh/;
 ssh-keygen -t rsa; cat ~/.ssh/id_rsa.pub | ssh $user@$host 'cat >> ~/.ssh/authorized_keys';
-ssh $user@$host 'chmod g-w,o-w ~/./ssh/; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys'
+ssh $user@$host 'chmod g-w,o-w ~/.ssh; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys'
