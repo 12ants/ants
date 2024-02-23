ed826ab6060da314ffcc4f2ce68c8ff5cb3052c8
Leon Ljunghorn <leonljunghorn@gmail.com>
2024-02-23 23:02:17 +0100

    ewffew

--- sh/cccc.sh
+++ sh/cccc.sh
@@ -125,7 +125,7 @@ c2="$cyan --$re"; tput civis;
 $1 $2 $3 $4 $pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2; PROC_ID=$!; while kill -0 "$PROC_ID"&>/dev/null; 
 do for X in "[        ]" "[$(tf)=$re       ]" "[$(tf)==$re      ]" "[$(tf)===$re     ]" "[$(tf)====    $re]"  "[ $(tf)====   $re]" \
 "[  $(tf)====$re  ]" "[   $(tf)==== $re]" "[    $(tf)====$re]" "[     "$(tf)"===$re]" "[      "$(tf)"=="$re"]" "[       =]" "[        ]" "[        ]" "[        ]"; 
-do echo -e "  [$(tb)  $re]$c2 Executing $rev $pro $re"$c2" $X"; tput cuu1; sleep 0.08; done; done;
+do echo -e "  [$(tb)  $re]$c2 Executing $rev $1 $2 $3 $4 $pro $re"$c2" $X"; tput cuu1; sleep 0.08; done; done;
 echo -e "\t\t\t\t\t\t [  "$green"DONE"$re"  ] \n\n\n\n\n"; tput cnorm;
 }
 ###########
